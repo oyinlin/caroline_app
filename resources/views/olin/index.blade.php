236ce9573bd52bac88f3259feb1aedb55ceb6dfe
oyinlin <stevany0714@gmail.com>
2023-09-12 17:17:38 +0700

    First commit

---
+++ resources/views/olin/index.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>senjana kopi</title>
+    <link rel="stylesheet" href="style.css">
+
+<!-- fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,
+wght@0,100;0,300;0,400;0,500;0,700;1,700&display=swap" rel="stylesheet">
+<!-- father icon -->
+<script src="https://unpkg.com/feather-icons"></script>
+<!-- my style -->
+   
+</head>
+<body>
+    <!-- navbar start -->
+    <nav class="navbar">
+       <a href="#" class="navbar-logo">senjana<span>kopi</span>.</a>
+       <div class="navbar-nav">
+        <a href="#home">Home</a>
+        <a href="#about">About Us</a>
+        <a href="#Menu">Menu</a>
+        <a href="#contact">Contact Person</a>
+       </div>
+        
+       <div class="navbar-extra">
+        <a href="#" id= "search"><i data-feather="search"></i></a>
+        <a href="#" id= "shopping-card"><i data-feather="shopping-cart"></i></a>
+        <a href="#" id= "menu"><i data-feather="menu"></i></a>
+        <a href="#" id= "hamburger-menu"><i data-feather="hamburger-menu"></i></a>
+       </div>
+    </nav>
+    <!-- navbar end -->
+<!-- hero start -->
+<section class="hero" id="home" >
+  <main class="content">
+    <h1>Mari Nikmati Secangkir <span>Kopi</span></h1>
+    <p>expreso adalah menu terbaik kami.</p>
+    <a href="beli.php" class="cta">nyok belik yok</a>
+  </main>
+</section>
+<!-- hero end -->
+<!-- about start  -->
+<section id="about" class="about">
+  <h2><span> About</span> us</h2>
+<div class="row">
+  <div class="about-img">
+    <img src="yiyi.jpg" alt="About Us">
+  </div>
+  <div class="content">
+    <h3>kenapa harus senjana kopi?</h3>
+    <p>Sejarah kopi merupakan sejarah yang meliputi penyebaran dan pengolahan tanaman kopi. 
+      Sejarah kopi mulai dicatat sejak abad ke-9. Awal mulanya kopi hanya ada di Ethiopia;
+       biji-bijian asli ditanam oleh orang Ethiopia dataran tinggi. Akan tetapi, ketika Bangsa Arab mulai memperluas 
+       perdagangannya, biji kopi pun meluas sampai ke Afrika Utara dan ditanam secara massal di sana. 
+      Dari Afrika Utara. biji kopi mulai meluas ke Asia hingga pasaran Eropa, dan ketenarannya sebagai minuman mulai menyebar.</p>
+
+      <P>Tanaman kopi dibawa masuk ke Indonesia pada masa kolonial Belanda, yang berhasil membuat Indonesia menjadi salah satu 
+        negara penghasil kopi utama di dunia hingga kini. Namun, akibat arus globalisasi dan kapitalisme Belanda yang diterima 
+        Indonesia, budaya kopi Indonesia hanya menjadi bagian dari keseharian dan tidak banyak diapresiasi masyarakat lokal.
+         Salah satu temuan penting adalah budaya kopi yang ada di Indonesia mendapatkan banyak pengaruh dari Eropa (Italia), 
+         Cina, Melayu, dan budaya lokal (seperti Jawa, Sumatra, dan lain-lain); baik dalam hal pengolahan maupun dalam penyajian.</P>
+
+      <p>Tahun 1957 M Catherine Uttley mendaftar ada 200 bar kopi di London. Mulai banyak yang bar kopi yang memakai plastik mulai
+         dari peralatan dapur, makan, lantai sampai furnitur. Kemudian tahun 1960 M Jumlah bar kopi tercatat bertambah dua kali lipat
+          dari 1,000 menjadi 2,000 di seluruh Inggris dimana terbanyak di London sekitar 500 buah. Dan tahun 1962 M puncak dari konsumsi
+           kopi per kapita di Amerika, 3 cangkir per orang per hari. Tahun 1962 M Perjanjian Internasional mengenai perdagangan kopi 
+           dibuat bertujuan untuk mengontrol harga. Dan tahun 1964 M bar kopi sekarat di Inggris, tergantikan oleh restoran dengan
+            berbagai hidangan. Kemudian tahun 1970 M Mokha café tutup setelah dikomplain sinis oleh penulis Amerika William S Burrough. 
+            Tahun 1971 M gerai Starbuck pertama dibuka di Seattle. Tahun1973 M Fair Trade Coffee pertama kali diimpor ke Eropa dari
+             Guatemala. Namun, tahun 1975 M Brazil menderita karena gagal panen, harga kopi dunia meroket. Dan tahun 1989 M Perjanjian
+              Kopi Internasional gagal menstabilkan harga. Tercatat dalam sejarah perdagangan kopi, harga pasaran kopi turun ke tingkat
+               yang paling rendah. Tahun 1990 M beberapa kedai kopi tutup karena penataan ruang (redevelopment) di Inggris.
+                Diperkenalkan organic coffee yang menjadi primadona di pasar kopi dunia. Dan tahun 1998 M jumlah gerai Starbuck
+                 mencapai 2.000 gerai di Amerika saja, dan dii seluruh dunia 5.715 gerai. Sedangkan di Indonesia telah dibuka
+                  sebanyak 11 gerai. Starbuck memosisikan diri sebagai kedai kopi dengan jaringan terbesar di seluruh dunia.
+                dan sekarang ada di kamiii</p>
+
+  </div>
+</div>
+</section>
+<!-- about end -->
+<!-- menu start -->
+<section id="Menu" class="menu">
+  <h2><span> Menu</span> kami</h2>
+  <p>ini adalah menu terbaik dari toko kami </p>
+
+  <div class="row">
+    <div class="menu-card">
+      <img src="xixi.jpg" 
+      alt="Espresso" class="menu-card-img">
+      <h3 class="menu-card-title">- Espresso -</h3>
+      <p class="menu-card-price">IDR 15k</p>
+    </div>
+    <div class="menu-card">
+      <img src="xixi.jpg" 
+      alt="Espresso" class="menu-card-img">
+      <h3 class="menu-card-title">- capuchino -</h3>
+      <p class="menu-card-price">IDR 15k</p>
+    </div>
+    <div class="menu-card">
+      <img src="xixi.jpg" 
+      alt="Espresso" class="menu-card-img">
+      <h3 class="menu-card-title">- Espresso -</h3>
+      <p class="menu-card-price">IDR 15k</p>
+    </div>
+    <div class="menu-card">
+      <img src="xixi.jpg" 
+      alt="Espresso" class="menu-card-img">
+      <h3 class="menu-card-title">- Espresso -</h3>
+      <p class="menu-card-price">IDR 15k</p>
+    </div>
+    <div class="menu-card">
+      <img src="xixi.jpg" 
+      alt="Espresso" class="menu-card-img">
+      <h3 class="menu-card-title">- Espresso -</h3>
+      <p class="menu-card-price">IDR 15k</p>
+    </div>
+  </div>
+</section>
+<!-- end -->
+<!-- contac start -->
+<section id="contact" class="contact"></section>
+<h2><span> Contact</span> Person</h2>
+<p>bisa di cek contac kami untuk pemesanan ataupun complain</p>
+
+<!-- contac end -->
+<!-- footer start -->
+<footer>
+  <div class="sosial">
+    <a href=""><i data-feather="instagram"></i></a>
+    <a href=""><i data-feather="twitter"></i></a>
+    <a href=""><i data-feather="facebook"></i></a>
+  </div>
+  <div class="links">
+    <a href="#home">Home</a>
+    <a href="#about">About Us</a>
+    <a href="#menu">Menu</a>
+    <a href="#contact">Contact Person</a>
+  </div>
+  <div class="credit">
+    <p>Created by <a href="">tepanicun</a>. |&copy; 2023.
+    </p>
+  </div>
+</footer>
+<!-- footer end -->
+<script>
+    // father icons
+      feather.replace()
+    </script> 
+</body>
+</html>
